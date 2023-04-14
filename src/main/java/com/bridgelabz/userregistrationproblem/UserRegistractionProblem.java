19e0a0542b8fd17f00cada0deed2bae7e13c4fdc
shridharuppar <upparshridhar@gmail.com>
2023-04-14 12:18:55 +0530

    Add: check the password

--- src/main/java/com/bridgelabz/userregistrationproblem/UserRegistractionProblem.java
+++ src/main/java/com/bridgelabz/userregistrationproblem/UserRegistractionProblem.java
@@ -8,30 +8,27 @@ public class UserRegistractionProblem {
 
 	public static void main(String[] args) {
 
-		String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
-		String regex1 = "^[1-9][0-9]\\s[1-9][0-9]{9}";
+		String regex = "^[1-9][0-9]\\s[1-9][0-9]{9}";
+		String regex1 = "^.{8,}$";
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Email Address: ");
-		String data = sc.nextLine();
 		System.out.println("Enter Mobile Number: ");
 		String mobileNO = sc.nextLine();
+		System.out.println("Enter A Password: ");
+		String password = sc.nextLine();
 
 		Pattern pattern = Pattern.compile(regex1);
-		Matcher matcher = pattern.matcher(data);
-		Matcher matcher1 = pattern.matcher(mobileNO);
+		Matcher matcher = pattern.matcher(mobileNO);
+		Matcher matcher1 = pattern.matcher(password);
 		boolean result = matcher1.matches();
 
 		if (result) {
-			System.out.println("Valid Email");
 			System.out.println("Valid Mobile Number");
+			System.out.println("You have entered a Valid Password");
 		} else {
-			System.out.println("Invalid Email");
 			System.out.println("Invalid Mobile Number");
+			System.out.println("Invalid Password");
 		}
-
 	}
 }
-
- 
 
