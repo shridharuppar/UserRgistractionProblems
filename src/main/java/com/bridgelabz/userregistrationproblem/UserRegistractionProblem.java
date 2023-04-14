7a6a142def52b14619d8e34b2b533936efed1e28
shridharuppar <upparshridhar@gmail.com>
2023-04-14 12:26:57 +0530

    Add : check the numeric password

--- src/main/java/com/bridgelabz/userregistrationproblem/UserRegistractionProblem.java
+++ src/main/java/com/bridgelabz/userregistrationproblem/UserRegistractionProblem.java
@@ -8,27 +8,25 @@ public class UserRegistractionProblem {
 
 	public static void main(String[] args) {
 
-	
+		String regex = "^(?=.*[A-Z]).{8,}$";
+		String regex1 = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
 
-			String regex = "^.{8,}$";
-			String regex1 = "^(?=.*[A-Z]).{8,}$";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a password having atleast one uppercase : ");
+		System.out.println("Enter a password having atleast one uppercase & one number : ");
+		String password = sc.nextLine();
 
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter A Password: ");
-			System.out.println("Enter a password having atleast one uppercase : ");
-			String password = sc.nextLine();
+		Pattern pattern = Pattern.compile(regex1);
+		Matcher matcher = pattern.matcher(password);
+		boolean result = matcher.matches();
 
-			Pattern pattern = Pattern.compile(regex1);
-			Matcher matcher = pattern.matcher(password);
-			boolean result = matcher.matches();
-
-			if (result) {
-				System.out.println("You have entered a Valid Password");
-			} else {
-				System.out.println("Invalid Password");
-			}
-
+		if (result) {
+			System.out.println("You have entered a Valid Password");
+		} else {
+			System.out.println("Invalid Password");
 		}
 	}
+		}
+	
 
 
