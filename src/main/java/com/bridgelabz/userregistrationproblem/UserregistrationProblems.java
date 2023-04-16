0ba9a1ebcf0b787ed73ba969aecfdc6a53af03b4
shridharuppar <upparshridhar@gmail.com>
2023-04-16 23:45:29 +0530

    add : To valid Email Samples

--- src/main/java/com/bridgelabz/userregistrationproblem/UserregistrationProblems.java
+++ src/main/java/com/bridgelabz/userregistrationproblem/UserregistrationProblems.java
@@ -6,24 +6,27 @@ public class UserregistrationProblems {
 	
 	
 
-		public static void main(String[] args) {
+	public static void main(String[] args) {
+		String regex = "^[a-zA-Z0-9]+([-\\+._][a-zA-Z0-9]+)*@[a-zA-Z0-9]+(\\.[a-zA-Z]{2,}){1,2}$";
 
-			String regex = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+		String regex1 = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).{8,}$";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a password having atleast 1 uppercase, 1 number & 1 special character  : ");
+		String password = sc.nextLine();
+		System.out.println("Enter Email Address: ");
+		String data = sc.nextLine();
 
-			String regex1 = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).{8,}$";
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter a password having atleast one uppercase & one number : ");
-			System.out.println("Enter a password having atleast 1 uppercase, 1 number & 1 special character  : ");
-			String password = sc.nextLine();
+		Pattern pattern = Pattern.compile(regex1);
+		Matcher matcher = pattern.matcher(password);
+		Matcher matcher1 = pattern.matcher(data);
+		boolean result = matcher1.matches();
 
-			Pattern pattern = Pattern.compile(regex1);
-			Matcher matcher = pattern.matcher(password);
-			boolean result = matcher.matches();
-
-			if (result) {
-				System.out.println("You have entered a Valid Password");
-			} else {
-				System.out.println("Invalid Password");
-			}
+		if (result) {
+			System.out.println("You have entered a Valid Password");
+			System.out.println("Valid Email");
+		} else {
+			System.out.println("Invalid Password");
+			System.out.println("Invalid Email");
 		}
-}
+
+	}}
